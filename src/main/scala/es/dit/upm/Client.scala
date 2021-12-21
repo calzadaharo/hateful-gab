c7ec9969901ecc5d5ce364eef7c2cbbeed4074be
calzadaharo@gmail.com <calzadaharo@gmail.com>
2021-12-21 12:57:06 +0100

    client implemented

--- src/main/scala/es/dit/upm/Client.scala
+++ src/main/scala/es/dit/upm/Client.scala
@@ -1,5 +1,11 @@
 package es.dit.upm
 
-object Client {
+import com.raphtory.algorithms.ConnectedComponents
+import com.raphtory.core.build.client.RaphtoryClient
+import es.dit.upm.algorithms.DepthFromOriginal
 
+object ExampleClient extends App {
+  val client = new RaphtoryClient("localhost:1600",1700)
+  client.pointQuery(DepthFromOriginal(path="/home/rodrigo/output/partitioned"),42977)
 }
+
