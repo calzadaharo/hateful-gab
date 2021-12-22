fcd98110dfce8c5c40518488167e1d18c949fc50
calzadaharo@gmail.com <calzadaharo@gmail.com>
2021-12-22 13:35:30 +0100

    fix Cient

--- src/main/scala/es/dit/upm/Client.scala
+++ src/main/scala/es/dit/upm/Client.scala
@@ -4,7 +4,7 @@ import com.raphtory.algorithms.ConnectedComponents
 import com.raphtory.core.build.client.RaphtoryClient
 import es.dit.upm.algorithms.DepthFromOriginal
 
-object ExampleClient extends App {
+object Client extends App {
   val client = new RaphtoryClient("localhost:1600",1700)
   client.pointQuery(DepthFromOriginal(path="/home/rodrigo/output/partitioned"),42977)
 }
