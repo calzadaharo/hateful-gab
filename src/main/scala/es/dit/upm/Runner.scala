e47225ad0111bf1414553327f8f39d9ca01f560a
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-01-12 12:33:28 +0100

    Server update

--- src/main/scala/es/dit/upm/Runner.scala
+++ src/main/scala/es/dit/upm/Runner.scala
@@ -10,20 +10,23 @@ import com.raphtory.spouts.FileSpout
 object Runner extends App {
 //  val source    = new FileSpout(
 //    "/home/rodrigo/Examples/hateful-gab/src/main/scala/es/dit/upm/data")
-  val source    = new FileSpout(
-    "/home/rcalzada/hateful-gab/src/main/scala/es/dit/upm/data")
-//  val source    = new HGSpoutHDFS(
-//    "hdfs://com31.dit.upm.es:9000/", "/data/rcalzada/datasets/definitive/hateful_gab.csv/")
+//  val source    = new FileSpout(
+//    "/home/rcalzada/hateful-gab/src/main/scala/es/dit/upm/data")
+  val source    = new HGSpoutHDFS(
+    "hdfs://com31.dit.upm.es:9000/", "/data/rcalzada/datasets/definitive/hateful_gab.csv/")
 
   val builder   = new HGGraphBuilder()
   val rg        = RaphtoryGraph[String](source,builder)
 
-//  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output"),42977)
-  rg.pointQuery(DepthFromOriginal(path="/home/rcalzada/output"),42977)
+//  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output"),timestamp = 42977)
+//  rg.pointQuery(DepthFromOriginal(path="/home/rcalzada/output"),42977)
 
   // Partition 0
 //  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output"),timestamp = 3062658)
 
-//   Partition 1
+  // Partition 1
 //  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output"),5505216)
+
+  // Whole dataset
+  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output"),timestamp = 46417964)
 }
