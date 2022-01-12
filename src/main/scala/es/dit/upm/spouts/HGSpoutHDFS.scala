7027f1ed8d07c7963a9f748eb61da6d4c06d316e
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-01-12 12:51:01 +0100

    Spout update

--- src/main/scala/es/dit/upm/spouts/HGSpoutHDFS.scala
+++ src/main/scala/es/dit/upm/spouts/HGSpoutHDFS.scala
@@ -1,4 +1,4 @@
-package com.raphtory.spouts
+package es.dit.upm.spouts
 
 import com.raphtory.core.components.spout.Spout
 
@@ -10,7 +10,7 @@ import com.typesafe.scalalogging.LazyLogging
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
-class HdfsSpout(headerUrlPort:String, route:String, dropHeader:Boolean=false) extends Spout[String] {
+class HdfsSpoutHDFS(headerUrlPort:String, route:String, dropHeader:Boolean=false) extends Spout[String] {
 
   val fs = FileSystem.get(new URI(headerUrlPort), new Configuration())
   private var status = fs.listStatus(new Path(route))
