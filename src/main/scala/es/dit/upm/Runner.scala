f812e196737b523531eb1bb513b39c7f936ab854
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-02-01 13:17:42 +0100

    Runner ready

--- src/main/scala/es/dit/upm/Runner.scala
+++ src/main/scala/es/dit/upm/Runner.scala
@@ -21,11 +21,11 @@ object Runner extends App {
 
   val month = 3600*24*30
 
-  val source    = new FileSpout(
-    "/home/rodrigo/Examples/hateful-gab/src/main/scala/es/dit/upm/data/",
-    "part-00000-hateful_gab.csv")
 //  val source    = new FileSpout(
-//    "/home/rcalzada/data/hateful_gab.csv")
+//    "/home/rodrigo/Examples/hateful-gab/src/main/scala/es/dit/upm/data/",
+//    "part-00000-hateful_gab.csv")
+  val source    = new FileSpout(
+    "/home/rcalzada/data/hateful_gab.csv")
 //  val source    = new HGSpoutHDFS(
 //    "hdfs://com31.dit.upm.es:9000/", "/data/rcalzada/datasets/definitive/hateful_gab.csv/")
 
@@ -43,13 +43,13 @@ object Runner extends App {
 //  rg.pointQuery(DepthFromOriginal(path="/home/rcalzada/output"),42977)
 
   // Partition 0
-  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output/new"),timestamp = 3062658)
+//  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output/new"),timestamp = 3062658)
 
   // Partition 1
 //  rg.pointQuery(DepthFromOriginal(path="/home/rodrigo/output"),5505216)
 
   // Whole dataset
-//  rg.pointQuery(DepthFromOriginal(path="/home/rcalzada/output"),timestamp = 46417964)
+  rg.pointQuery(DepthFromOriginal(path="/home/rcalzada/output/new"),timestamp = 46417964)
 
   //----------------------------------------------------------------------------------------------------
   //----------------------------------------------------------------------------------------------------
