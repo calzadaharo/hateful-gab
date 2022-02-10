1f51d883791708b9b0b53c3c2b727775941fdf6d
calzadaharo@gmail.com <calzadaharo@gmail.com>
2022-02-10 12:55:39 +0100

    Fixed: DTfO works properly

--- src/main/scala/es/dit/upm/algorithms/DepthTimestampFromOriginal.scala
+++ src/main/scala/es/dit/upm/algorithms/DepthTimestampFromOriginal.scala
@@ -7,57 +7,66 @@ class DepthTimestampFromOriginal(path: String) extends GraphAlgorithm{
     graph
       .step ({
         vertex =>
-          if (vertex.getAllNeighbours().length == 0) {
-            vertex.voteToHalt
-          } else if (vertex.getAllNeighbours().length > 0 &&
-            vertex.getPropertyOrElse("type", null) == "original") {
-            vertex.setState("cascade", vertex.ID)
-            vertex.setState("depth",0)
-            vertex.setState("index", 0)
-            val timestamp = vertex.getProperty("timestamp")
-            vertex.messageAllIngoingNeighbors(vertex.ID,timestamp,0,true)
+          if (vertex.getAllNeighbours().isEmpty) {
             vertex.voteToHalt
           } else {
-            val timestamp = vertex.getProperty("timestamp")
-            vertex.setState("index", 0)
-            vertex.messageAllOutgoingNeighbors(0,timestamp,0,false)
+            if (vertex.getPropertyOrElse("type", null) == "original") {
+              vertex.setState("cascade", vertex.ID)
+              vertex.setState("level",0)
+              vertex.setState("index", 0)
+              val timestamp = vertex.getPropertyOrElse("timestamp","0").toLong
+              vertex.messageAllIngoingNeighbors(vertex.ID,timestamp,0,true)
+            } else {
+              val timestamp = vertex.getPropertyOrElse("timestamp","0").toLong
+              println("vertex: "+vertex.ID + ", neighbors: "+vertex.getOutNeighbours())
+              vertex.setState("index", 0)
+              vertex.messageAllOutgoingNeighbors(0.toLong,timestamp,0,false)
+            }
           }
       })
 
       .iterate ({
         vertex =>
           val messages = vertex.messageQueue[(Long,Long,Int,Boolean)]
-          val cascade = messages(0)._1
-          val timestamp = messages(0)._2
-          val level = messages(0)._3 + 1
-          val flag = messages(0)._4
-          val index = vertex.getStateOrElse[Int]("index",0) + 1
-
-          // Check if message is already descending
-          if (flag) {
-            // Check if the message was born in the original
-            if (cascade != 0) {
-              vertex.setState("cascade",cascade)
-              vertex.setState("level",level)
-              vertex.setState("index",index)
-              vertex.messageAllIngoingNeighbors(cascade,timestamp,level,true)
-            } else {
-              // Check if vertex timestamp is higher than the message one
-              if (timestamp < vertex.getPropertyOrElse[Long]("timestamp",0)) {
+          messages.foreach(message => {
+            val cascade = message._1
+            val timestamp = message._2
+            val level = message._3 + 1
+            val flag = message._4
+            val index = vertex.getStateOrElse[Int]("index",0) + 1
+            
+            println("MESSAGE: "+flag+", vertex: "+ vertex.ID)
+            // Check if message is already descending
+            if (flag) {
+              // Check if the message was born in the original
+              if (cascade != 0) {
+                println("Update cascade: "+cascade+", "+index)
+                vertex.setState("cascade",cascade)
+                vertex.setState("level",level)
                 vertex.setState("index",index)
+                vertex.messageAllIngoingNeighbors(cascade,timestamp,level,true)
+              } else {
+                // Check if vertex timestamp is higher than the message one
+                println(timestamp + " " + vertex.getPropertyOrElse("timestamp","0").toLong)
+                if (timestamp < vertex.getPropertyOrElse("timestamp","0").toLong) {
+                  println("Update only index: "+index)
+                  vertex.setState("index",index)
+                }
+                // Common for both higher and lower than timestamp
+                vertex.messageAllIngoingNeighbors(0.toLong,timestamp,0,true)
               }
-              // Common for both higher and lower than timestamp
-              vertex.messageAllIngoingNeighbors(0,timestamp,0,true)
-            }
-          // Message ascending
-          } else {
-            // Check is message has reached the original
-            if (vertex.getPropertyOrElse("type",null) == "original"){
-              vertex.messageAllIngoingNeighbors(0,timestamp,0,true)
+              // Message ascending
             } else {
-              vertex.messageAllOutgoingNeighbors(0,timestamp,0,false)
+              // Check is message has reached the original
+              if (vertex.getPropertyOrElse("type",null) == "original"){
+                println("REACH ORIGINAL: "+timestamp)
+                vertex.messageAllIngoingNeighbors(0.toLong,timestamp,0,true)
+              } else {
+                println("NOT ORIGINAL: "+timestamp)
+                vertex.messageAllOutgoingNeighbors(0.toLong,timestamp,0,false)
+              }
             }
-          }
+          })
       }, iterations = 100000, executeMessagedOnly = true)
 
       .select ({
